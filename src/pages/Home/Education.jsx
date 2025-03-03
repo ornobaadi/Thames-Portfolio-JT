df11ba006b58f61941015a37f648b8ad1632b1b8
ornobaadi <aadi4789@gmail.com>
2025-03-03 16:03:48 +0600

    Firebase favicon spacing

--- src/pages/Home/Education.jsx
+++ src/pages/Home/Education.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FaGraduationCap } from 'react-icons/fa';
+import { MdOutlineKeyboardDoubleArrowUp } from "react-icons/md";
 
 const Education = () => {
     const educationData = [
@@ -24,7 +25,7 @@ const Education = () => {
     ];
 
     return (
-        <section className="py-16 px-4 bg-white">
+        <section className="py-16 px-60 bg-white">
             <div className="container mx-auto">
                 <div className="flex flex-col lg:flex-row justify-between gap-10">
                     {/* Left Content */}
@@ -78,11 +79,9 @@ const Education = () => {
             <div className="fixed bottom-6 right-6">
                 <button
                     onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-                    className="w-12 h-12 bg-lime-500 rounded-full flex items-center justify-center text-white shadow-lg"
+                    className="w-12 h-12 cursor-pointer bg-lime-500 rounded-full flex items-center justify-center text-white shadow-lg"
                 >
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 10l7-7m0 0l7 7m-7-7v18" />
-                    </svg>
+                    <MdOutlineKeyboardDoubleArrowUp className='text-3xl' />
                 </button>
             </div>
         </section>
